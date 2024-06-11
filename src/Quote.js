80b9520927faa6cff3366ffcb1086bed7e952bdc
kristina.perova <chris.perowa@gmail.com>
2024-06-11 19:00:12 +0200

    deleted [bgColor] on oder to bgColor was not white when updating page

--- src/Quote.js
+++ src/Quote.js
@@ -13,7 +13,24 @@ const possibleQuotes = [
     '"Every day brings new choices." - Martha Beck',
     '"When you are enthusiastic about what you do, you feel this positive energy. It\'s very simple." - Paulo Coelho',
     '"Adopting the right attitude can convert a negative stress into a positive one." - Hans Selye',
-    '"I confused things with their names: that is belief." - Jean-Paul Sartre'
+    '"I confused things with their names: that is belief." - Jean-Paul Sartre',
+    '"Be yourself; everyone else is already taken." - Oscar Wilde',
+    '"I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best." - Marilyn Monroe',
+    '"Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe." - Albert Einstein',
+    '"If you tell the truth, you don\'t have to remember anything." - Mark Twain',
+    '"To live is the rarest thing in the world. Most people exist, that is all." - Oscar Wilde',
+    '"Life is what happens to us while we are making other plans." - Allen Saunders',
+    '"I have not failed. I\'ve just found 10,000 ways that won\'t work." - Thomas A. Edison',
+    '"Everything you can imagine is real." - Pablo Picasso',
+    '"Well done is better than well said." - Benjamin Franklin',
+    '"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart." - Helen Keller',
+    '"Be yourself; everyone else is already taken." - Oscar Wilde',
+    '"It is during our darkest moments that we must focus to see the light." - Aristotle',
+    '"Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate: only love can do that." - Martin Luther King Jr',
+    '"Well done is better than well said." - Benjamin Franklin',
+    '"You will face many defeats in life, but never let yourself be defeated." - Maya Angelou',
+    '"Never let the fear of striking out keep you from playing the game." - Babe Ruth',
+    '"Many of life\'s failures are people who did not realize how close they were to success when they gave up." - Thomas A. Edison'
 ]
 
 const generateRandomColor = () => {
@@ -30,14 +47,14 @@ const generateRandomColor = () => {
 }
 
 const QuoteBox = () => {
-    const [quote, setQuote] = useState(possibleQuotes[0]); // initialisation of state; quote - using for keeping the value, setQuote - using for updating
+    const [quote, setQuote] = useState(''); // initialisation of state; quote - using for keeping the value, setQuote - using for updating
     const [bgColor, setBgColor] = useState(generateRandomColor());
     let authorIdx = quote.split("-").length - 1
 
     useEffect(() => {
         document.body.style.backgroundColor = bgColor;
         getRandomQuote();
-    }, [bgColor])
+    });
 
     const getRandomQuote = () => {
         const randomIndex = Math.floor(Math.random() * possibleQuotes.length);
@@ -51,7 +68,7 @@ const QuoteBox = () => {
     // }
 
     const handleCLick = () => {
-        getRandomQuote();
+        // getRandomQuote();
         setBgColor(generateRandomColor());
     }
 
